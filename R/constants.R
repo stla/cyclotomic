ee0c8baed342ddf8c8846c93253dc9f6a9457cbc
Stéphane Laurent <laurent_step@yahoo.fr>
2023-04-13 09:28:51 +0200

    im

---
+++ R/constants.R
@@ -0,0 +1,5 @@
+#' @title The square root of -1
+#' @description The square root of -1 as a cyclotomic number.
+#' @return A cyclotomic number, the square root of -1.
+#' @export
+im <- function() e(4L)

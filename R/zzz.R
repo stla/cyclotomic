aa3397b3a92a532388855a6546ca5d16e172c28c
Stéphane Laurent <laurent_step@yahoo.fr>
2023-04-20 14:36:39 +0200

    removed Rcpp stuff

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,4 @@
 .onLoad <- function(libname, pkgname) {
-  R_extraneousPowers     <<- memoise::memoise(R_extraneousPowers)
-  R_phiNrpSqfree         <<- memoise::memoise(R_phiNrpSqfree)
-  R_squareFreeOddFactors <<- memoise::memoise(R_squareFreeOddFactors)
   extraneousPowers     <<- memoise::memoise(extraneousPowers)
   phiNrpSqfree         <<- memoise::memoise(phiNrpSqfree)
   squareFreeOddFactors <<- memoise::memoise(squareFreeOddFactors)
